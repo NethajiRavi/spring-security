b759ea0feeac79618805127c39fdd2f37d60f098
unknown <nethaji043@gmail.com>
2025-09-23 07:21:05 +0530

    cors and csrf fix

--- src/main/java/com/project/springseurity/config/ProdSecurityConfig.java
+++ src/main/java/com/project/springseurity/config/ProdSecurityConfig.java
@@ -3,13 +3,23 @@ package com.project.springseurity.config;
 
 import com.project.springseurity.exceptionhandling.CustomAccessDeniedHandler;
 import com.project.springseurity.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import com.project.springseurity.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -26,12 +36,32 @@ public class ProdSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.sessionManagement(smc->smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true)).
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler =  new CsrfTokenRequestAttributeHandler();
+        http.securityContext(context->context.requireExplicitSave(false)).
+                sessionManagement(sessionConfig->sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsConfig-> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                        corsConfiguration.setAllowCredentials(true);
+                        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                        corsConfiguration.setMaxAge(3600L);
+                        return  corsConfiguration;
+                    }
+                })).
+            //    sessionManagement(smc->smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true)).
                 requiresChannel(rcc->rcc.anyRequest().requiresSecure()). // only https request accept in production
 
 //        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
 //        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-        csrf(csrfConfig-> csrfConfig.disable())
+        csrf(csrfConfig->
+        csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                .ignoringRequestMatchers("/myContact","/myNotification")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+
                         .authorizeHttpRequests((requests) -> requests
                                 .requestMatchers("/myAccount","/myBalance","myCards","myLoans").authenticated()
                                 .requestMatchers("/myContact","/myNotification","/error","/register").permitAll());
--- src/main/java/com/project/springseurity/config/SecurityConfig.java
+++ src/main/java/com/project/springseurity/config/SecurityConfig.java
@@ -3,13 +3,23 @@ package com.project.springseurity.config;
 
 import com.project.springseurity.exceptionhandling.CustomAccessDeniedHandler;
 import com.project.springseurity.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import com.project.springseurity.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -28,15 +38,31 @@ public class SecurityConfig {
 
 //        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
 //        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler =  new CsrfTokenRequestAttributeHandler();
+        http.securityContext(context->context.requireExplicitSave(false)).
+                sessionManagement(sessionConfig->sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsConfig-> corsConfig.configurationSource(new CorsConfigurationSource() {
 
-        http.sessionManagement(smc->smc.invalidSessionUrl("/invalidSession").maximumSessions(1)
-                        .maxSessionsPreventsLogin(true)).
-                requiresChannel(rcc->rcc.anyRequest().requiresInsecure()). // only http
-
-                csrf(csrfConfig-> csrfConfig.disable())
-                        .authorizeHttpRequests((requests) -> requests
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration corsConfiguration = new CorsConfiguration();
+                corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                corsConfiguration.setAllowCredentials(true);
+                corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                corsConfiguration.setMaxAge(3600L);
+                return  corsConfiguration;}}))
+//                        .sessionManagement(smc->smc.invalidSessionUrl("/invalidSession").maximumSessions(1)
+//                        .maxSessionsPreventsLogin(true))
+                        .requiresChannel(rcc->rcc.anyRequest().requiresInsecure()). // only http
+                csrf(csrfConfig->
+                csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                        .ignoringRequestMatchers("/myContact","/myNotification")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests((requests) -> requests
                                 .requestMatchers("/myAccount","/myBalance","myCards","myLoans").authenticated()
-                                .requestMatchers("/myContact","/myNotification","/error","/register").permitAll());
+                                .requestMatchers("/myContact","/myNotification","/error","/register","/invalidSession").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint( new CustomBasicAuthenticationEntryPoint()));
         http.exceptionHandling(ehc-> new CustomAccessDeniedHandler()) ;
